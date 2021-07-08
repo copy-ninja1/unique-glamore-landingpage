60fc1f8336347e922b92500f42777f86c0e92019
copy-ninja1 <emmajnr1000@gmail.com>
2021-07-08 11:11:36 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      // Build your palette here
+      white: colors.white,
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.sky,
+      yellow: colors.amber,
+      teal: colors.teal
+
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
